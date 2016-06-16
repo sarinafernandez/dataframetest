244a4a78a69ee94baf3745173a345dd1057b8453
sarinafernandez <sarinafernandez5@gmail.com>
2016-06-16 15:13:54 -0700

    Add files via upload

--- ui.R
+++ ui.R
@@ -2,19 +2,16 @@
 
 shinyUI(fluidPage(
   titlePanel("Data Structure"),
-  sidebarLayout(
-    sidebarPanel( "This app will display the structure of your .csv data set"),
-    mainPanel()
-  ),
      
   fluidRow(
     
-    column(2,
+    column(3,
            fileInput("file", label = h4(".csv file input"))),
           br(),
     
-    column(2,
-           actionButton("action", label = "Display data structure"),
-           br())
+    column(3,
+           br(),
+           actionButton("action", label = "Display data structure")
+          )
 )
 ))
