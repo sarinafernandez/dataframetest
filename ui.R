ee75d44b14c72a6c6d724e261b98ec98d278c024
sarinafernandez <sarinafernandez5@gmail.com>
2016-06-16 09:59:21 -0700

    Initial project ui.R and server.R files.

---
+++ ui.R
@@ -0,0 +1,10 @@
+# ui.R
+
+shinyUI(fluidPage(
+  titlePanel("Data Structure"),
+  
+  sidebarLayout(
+    sidebarPanel( "This app displays the structure of your data blah blah"),
+    mainPanel("Load your .csv file")
+  )
+))
