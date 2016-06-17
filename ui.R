c453fc24bcc673128afa23f294d677ec84bf88bb
wasfazad <wasfazad@bu.edu>
2016-06-17 09:20:07 -0700

    edit server.R and ui.R

--- ui.R
+++ ui.R
@@ -1,17 +1,28 @@
-# ui.R
+library(shiny)
 
 shinyUI(fluidPage(
-  titlePanel("Data Structure"),
-     
-  fluidRow(
-    
-    column(3,
-           fileInput("file", label = h4(".csv data.frame file input"))),
-          br(),
-    
-    column(3,
-           br(),
-           actionButton("actionButton", label = "Display str output")
-          )
-)
+  titlePanel("Uploading Files"),
+  sidebarLayout(
+    sidebarPanel(
+      fileInput('file1', 'Choose CSV File',
+                accept=c('text/csv', 
+                         'text/comma-separated-values,text/plain', 
+                         '.csv')),
+      tags$hr(),
+      checkboxInput('header', 'Header', TRUE),
+      radioButtons('sep', 'Separator',
+                   c(Comma=',',
+                     Semicolon=';',
+                     Tab='\t'),
+                   ','),
+      radioButtons('quote', 'Quote',
+                   c(None='',
+                     'Double Quote'='"',
+                     'Single Quote'="'"),
+                   '"')
+    ),
+    mainPanel(
+      tableOutput('contents')
+    )
+  )
 ))
