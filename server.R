a34ad28d3b1d3d16844fa9d86dab80b0943f1a8f
wasfazad <wasfazad@bu.edu>
2016-06-17 11:30:12 -0700

    added reactive button

--- server.R
+++ server.R
@@ -1,20 +1,26 @@
 library(shiny)
 
 shinyServer(function(input, output) {
-  output$contents <- renderTable({
+  eventReactive(input$clicks, {
     
+  })
+  
+  output$contents <- renderTable({
+    data <- eventReactive(input$clicks, {input$file1})
     # input$file1 will be NULL initially. After the user selects
     # and uploads a file, it will be a data frame with 'name',
     # 'size', 'type', and 'datapath' columns. The 'datapath'
     # column will contain the local filenames where the data can
     # be found.
     
-    inFile <- input$file1
+    inFile <- data()
     
     if (is.null(inFile))
       return(NULL)
     
-    read.csv(inFile$datapath, header=input$header, sep=input$sep, 
-             quote=input$quote)
+     read.csv(inFile$datapath)
+     
+     
+     
   })
 })
